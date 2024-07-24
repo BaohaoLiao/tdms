d690fb432de28f074764ea0ffc79c0b232b56a50
baohaoliao <liaobaohao@gmail.com>
2024-07-24 22:18:09 +0200

    debug

--- models/__init__.py
+++ models/__init__.py
@@ -3,4 +3,5 @@ from models.standard_model import StandardModel
 MODEL_FACTORY = {
     "densenet201": StandardModel,
     "tf_efficientnet_b3.ns_jft_in1k": StandardModel,
+    "resnet34": StandardModel,
 }
