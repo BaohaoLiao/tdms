84f5b1389b6909b34e7696b4fbbd83a3444c2bed
baohaoliao <liaobaohao@gmail.com>
2024-07-25 01:21:17 +0200

    add model

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,7 @@ MODEL_FACTORY = {
     "densenet201": StandardModel,
     "tf_efficientnet_b3.ns_jft_in1k": StandardModel,
     "tf_efficientnet_b4.ns_jft_in1k": StandardModel,
+    "tf_efficientnet_b5.ns_jft_in1k": StandardModel,
     "tf_efficientnetv2_b3.in21k_ft_in1k": StandardModel,
     "resnet34": StandardModel,
 }
