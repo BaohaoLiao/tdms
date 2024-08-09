3e040198b03c20212814129b5da62672c13ed0df
baohaoliao <liaobaohao@gmail.com>
2024-08-09 15:22:41 +0200

    data process, no alias

--- models/standard_model.py
+++ models/standard_model.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 import timm
 
 class StandardModel(nn.Module):
-    def __init__(self, model_name, in_c=30, n_classes=75, pretrained=False, features_only=False):
+    def __init__(self, model_name, in_c=30, n_classes=75, pretrained=False, features_only=False, **kwargs):
         super().__init__()
         self.model = timm.create_model(
                                     model_name,
@@ -11,7 +11,7 @@ class StandardModel(nn.Module):
                                     in_chans=in_c,
                                     num_classes=n_classes,
                                     global_pool='avg'
-                                    )
+                                    **kwargs)
     
     def forward(self, x):
         y = self.model(x)
